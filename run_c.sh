672e7e9e739ae26db06e4d1de96a2550934b4fff
zachs1326 <zachs1326@gmail.com>
2025-03-04 22:18:15 -0500

    adding the ip in the correct place, still just localhost

--- run_c.sh
+++ run_c.sh
@@ -9,28 +9,28 @@ cleanup() {
 trap cleanup EXIT
 
 # Compile c programs
-gcc bandwidth_hog.c 127.0.0.1  -o bandwidth_hog
-gcc bandwidth_hog_burst.c 127.0.0.1 -o bandwidth_hog_burst
+gcc bandwidth_hog.c -o bandwidth_hog
+gcc bandwidth_hog_burst.c -o bandwidth_hog_burst
 gcc cpu_hog.c -o cpu_hog
 gcc disk_hog.c -o disk_hog
 gcc memory_hog.c -o memory_hog
 gcc memory_hog_leak.c -o memory_hog_leak
 
 # Start all of the files in the background
-./bandwidth_hog &
+./bandwidth_hog 127.0.0.1  &
 BANDWIDTH_PID=$!
 
-./bandwidth_hog_burst &
+./bandwidth_hog_burst 127.0.0.1  &
 BURST_PID=$!
 
-./cpu_hog &
+./cpu_hog 127.0.0.1 &
 CPU_PID=$!
 
-./disk_hog &
+./disk_hog 127.0.0.1 &
 DISK_PID=$!
 
-./memory_hog &
+./memory_hog 127.0.0.1 &
 MEMORY_PID=$!
 
-./memory_hog_leak &
+./memory_hog_leak 127.0.0.1 &
 LEAK_PID=$!
