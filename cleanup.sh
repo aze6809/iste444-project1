6c272e9422beaebdd455010837274472a0a105b8
zachs1326 <zachs1326@gmail.com>
2025-03-06 19:33:54 -0500

    changing the cleanup.sh to remove the .csv files

--- cleanup.sh
+++ cleanup.sh
@@ -5,7 +5,8 @@
 
 echo "Cleaning up compiled binaries..."
 
-rm -f bandwidth_hog bandwidth_hog_burst cpu_hog disk_hog memory_hog memory_hog_leak process_metrics.csv
+rm -f bandwidth_hog bandwidth_hog_burst cpu_hog disk_hog memory_hog memory_hog_leak
+rm -f bandwidth_hog_metrics.csv bandwidth_hog_burst_metrics.csv cpu_hog_metrics.csv disk_hog_metrics.csv memory_hog_metrics.csv memory_hog_leak_metrics.csv
 
 echo "Cleanup complete!"
 ls  -al
