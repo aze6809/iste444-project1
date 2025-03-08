514fbb82b4a23a6d7f98515b15ed57e665f52986
komeally <kasim.omeally@gmail.com>
2025-03-07 21:29:51 -0500

    Fixed spelling error in init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 # Pull this to RLES, then add to the git ignore so i can just run this 
 # for the chmod on the apm_tool.sh
 
-chmod 755 amp_tool.sh && chmod +x apm_tool.sh
+chmod 755 apm_tool.sh && chmod +x apm_tool.sh
